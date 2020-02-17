b1cf7e83bcf80214719db7f7a6e7ee72cb0cbd44
Pratyush Yadav <p.yadav@ti.com>
2020-02-17 13:59:31 +0530

    spi: spi-mem: allow specifying a command's extension
    In xSPI mode, flashes expect 2-byte opcodes. The second byte is called
    the "command extension". There can be 3 types of extensions in xSPI:
    repeat, invert, and hex. When the extension type is "repeat", the same
    opcode is sent twice. When it is "invert", the second byte is the
    inverse of the opcode. When it is "hex" an additional opcode byte based
    is sent with the command whose value can be anything.
    
    So, make opcode a 16-bit value and add a 'nbytes', similar to how
    multiple address widths are handled.
    
    Signed-off-by: Pratyush Yadav <p.yadav@ti.com>

--- include/spi-mem.h
+++ include/spi-mem.h
@@ -72,6 +72,8 @@ enum spi_mem_data_dir {
 
 /**
  * struct spi_mem_op - describes a SPI memory operation
+ * @cmd.nbytes: number of opcode bytes (only 1 or 2 are valid). The opcode is
+ *		sent MSB-first.
  * @cmd.buswidth: number of IO lines used to transmit the command
  * @cmd.opcode: operation opcode
  * @cmd.dtr: whether the command opcode should be sent in DTR mode or not
@@ -95,9 +97,10 @@ enum spi_mem_data_dir {
  */
 struct spi_mem_op {
 	struct {
+		u8 nbytes;
 		u8 buswidth;
-		u8 opcode;
 		u8 dtr : 1;
+		u16 opcode;
 	} cmd;
 
 	struct {
